4bbd8f7108585d6a424557380ad0faa6146a6107
pol <pol.beauval@gmail.com>
2025-03-16 11:42:53 +0000

    initialisation

---
+++ nextjs-dashboard/app/customers/page.tsx
@@ -0,0 +1,4 @@
+export default function Page() {
+    return (<div><p>login Page</p>
+    <h1>toto</h1></div>);
+  }
---
+++ nextjs-dashboard/app/customers/totutu/page.tsx
@@ -0,0 +1,4 @@
+export default function Page() {
+    return (<div><p>totutu Page</p>
+    <h1>titatitutu</h1></div>);
+  }
